a6cb776ef7bc95ff73d3a5755a1d1ffce41eff77
AhmdShuvo <tanzirshovo2@gmail.com>
2023-02-12 21:26:40 +0600

    environment variables

--- app/Index.js
+++ app/Index.js
@@ -4,15 +4,10 @@ const express=require('express');
 const cors=require('cors');
 require("dotenv").config();
 
-
 const app=(express())
 
 
-
-
-
 const port =process.env.PORT || 9000;
-
 
 app.use(express.json())
 app.use(cors())
